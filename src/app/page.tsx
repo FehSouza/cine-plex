dac5d45834a728bc37e785d0316f3450b49c3023
FehSouza <engcivil.fernandasouza@gmail.com>
2023-07-14 22:14:35 -0300

    teste

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,17 @@
-import Carousel from './_components/Carousel'
 import MainBanner from './_components/MainBanner'
-import { getBestMovies, getPopular, getUpcoming } from './services'
+import { getBestMovies } from './services'
 import S from './styles.module.scss'
 
 export default async function Home() {
-  const [bestMovies, nowPlaying, upcoming] = await Promise.all([getBestMovies(), getUpcoming(), getPopular()])
+  const [bestMovies] = await Promise.all([getBestMovies()])
 
   return (
     <main className={S.main}>
       <div className={S.content}>
         <MainBanner movies={bestMovies} />
-        <Carousel title="Em Cartaz" movies={nowPlaying} />
+        {/* <Carousel title="Em Cartaz" movies={nowPlaying} /> */}
         <hr className={S.division} />
-        <Carousel title="Em Breve" movies={upcoming} upcoming={true} />
+        {/* <Carousel title="Em Breve" movies={upcoming} upcoming={true} /> */}
         <hr className={S.division} />
         {/* <Carousel title="Nossas Sugestões" movies={popular} /> */}
       </div>
