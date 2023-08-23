79006866a04ffd71fb01b387766fcdd3254941ef
FehSouza <engcivil.fernandasouza@gmail.com>
2023-08-23 20:38:31 -0300

    feat(cast-crew-list): tab buttons

---
+++ src/app/_components/TeamTab/index.tsx
@@ -0,0 +1,120 @@
+'use client'
+
+import { Cast, Crew, MovieCredits } from '@/@types'
+import { DICTIONARY_CREW_DEPARTMENT } from '@/dictionary'
+import { useState } from 'react'
+import TeamCard from '../TeamCard'
+import S from './styles.module.scss'
+
+interface TeamTabProps {
+  credits: MovieCredits
+}
+
+export const TeamTab = ({ credits }: TeamTabProps) => {
+  const [tabActive, setTabActive] = useState('cast')
+
+  const setCast = new Set()
+  const setCrew = new Set()
+
+  const removeDuplicates = ({ item, set }: { item: Cast | Crew; set: Set<unknown> }) => {
+    const duplicated = set.has(item.id)
+    set.add(item.id)
+    return !duplicated
+  }
+
+  const castList = credits.cast.filter((cast) => removeDuplicates({ item: cast, set: setCast }))
+  const crewList = credits.crew.filter((crew) => removeDuplicates({ item: crew, set: setCrew }))
+
+  const crewListSorted = crewList.sort(function (a, b) {
+    if (a.job < b.job) return -1
+    if (a.job > b.job) return 1
+    return 0
+  })
+
+  let departments = [] as string[]
+
+  crewListSorted.forEach((item) => {
+    const depart = item.department
+    if (!departments.includes(depart)) departments.push(depart)
+  })
+
+  const crewListFormatted = departments.sort().map((depart) => {
+    const filter = crewListSorted.filter((item) => item.department === depart)
+    return [depart, filter]
+  })
+
+  const handleSelectTab = (tabName: string) => {
+    setTabActive(tabName)
+    window.scrollTo({ top: 0, behavior: 'smooth' })
+  }
+
+  return (
+    <>
+      <section className={[S.container, S.containerListNames].join(' ')}>
+        <button className={[S.listNameTab, `${tabActive === 'cast' && S.active}`].join(' ')} onClick={() => handleSelectTab('cast')}>
+          Elenco <span className={S.listQuant}>{`(${castList.length})`}</span>
+        </button>
+
+        <button className={[S.listNameTab, `${tabActive === 'crew' && S.active}`].join(' ')} onClick={() => handleSelectTab('crew')}>
+          Equipe Técnica <span className={S.listQuant}>{`(${crewList.length})`}</span>
+        </button>
+
+        <button className={[S.listNameTab, `${tabActive === 'all' && S.active}`].join(' ')} onClick={() => handleSelectTab('all')}>
+          Todos <span className={S.listQuant}>{`(${castList.length + crewList.length})`}</span>
+        </button>
+      </section>
+
+      <section className={[S.container, S.containerLists].join(' ')}>
+        {(tabActive === 'cast' || tabActive === 'all') && (
+          <div className={S.listContent}>
+            {tabActive === 'all' && <h2 className={S.listName}>Elenco</h2>}
+
+            <ul className={S.listWrapper}>
+              {castList.map((item) => {
+                const id = item.id
+                const image = item.profile_path
+                const name = item.name
+                const subName = item.character
+
+                return <TeamCard key={`cast-${id}`} id={id} image={image} name={name} subName={subName} />
+              })}
+            </ul>
+          </div>
+        )}
+
+        {tabActive === 'all' && <hr className={S.division} />}
+
+        {(tabActive === 'crew' || tabActive === 'all') && (
+          <div className={S.listContent}>
+            {tabActive === 'all' && <h2 className={S.listName}>Equipe Técnica</h2>}
+
+            {crewListFormatted.map((depart) => {
+              const name = depart[0] as keyof typeof DICTIONARY_CREW_DEPARTMENT
+              const list = depart[1] as Crew[]
+
+              return (
+                <div key={`department-${name}`} className={S.listDepart}>
+                  <h3 className={S.departName}>
+                    {DICTIONARY_CREW_DEPARTMENT[name] ? DICTIONARY_CREW_DEPARTMENT[name] : name}
+                    <span className={S.listDepartQuant}>{`(${list.length})`}</span>
+                  </h3>
+
+                  <ul className={S.listWrapper}>
+                    {list.map((item) => {
+                      const id = item.id
+                      const image = item.profile_path
+                      const name = item.name
+                      const subName = item.job
+
+                      return <TeamCard key={`crew-${id}`} id={id} image={image} name={name} subName={subName} />
+                    })}
+                  </ul>
+                </div>
+              )
+            })}
+          </div>
+        )}
+      </section>
+    </>
+  )
+}
--- src/app/filme/[id]/elenco/page.tsx
+++ src/app/filme/[id]/elenco/page.tsx
@@ -1,7 +1,4 @@
-import { Cast, Crew } from '@/@types'
-import { TMDBPosterLoader } from '@/app/_components'
-import TeamCard from '@/app/_components/TeamCard'
-import { DICTIONARY_CREW_DEPARTMENT } from '@/dictionary'
+import { TMDBPosterLoader, TeamTab } from '@/app/_components'
 import { getCreditsMovie, getMovie } from '@/services'
 import Image from 'next/image'
 import Link from 'next/link'
@@ -19,39 +16,10 @@ export default async function Elenco({ params }: ElencoProps) {
 
   const poster = movie.poster_path
   const title = movie.title
-  const setCast = new Set()
-  const setCrew = new Set()
-
-  const removeDuplicates = ({ item, set }: { item: Cast | Crew; set: Set<unknown> }) => {
-    const duplicated = set.has(item.id)
-    set.add(item.id)
-    return !duplicated
-  }
-
-  const castList = credits.cast.filter((cast) => removeDuplicates({ item: cast, set: setCast }))
-  const crewList = credits.crew.filter((crew) => removeDuplicates({ item: crew, set: setCrew }))
-
-  const crewListSorted = crewList.sort(function (a, b) {
-    if (a.job < b.job) return -1
-    if (a.job > b.job) return 1
-    return 0
-  })
-
-  let departments = [] as string[]
-
-  crewListSorted.forEach((item) => {
-    const depart = item.department
-    if (!departments.includes(depart)) departments.push(depart)
-  })
-
-  const crewListFormatted = departments.sort().map((depart) => {
-    const filter = crewListSorted.filter((item) => item.department === depart)
-    return [depart, filter]
-  })
 
   return (
     <main className={S.main}>
-      <section className={[S.container, S.containerInfoBar].join(' ')}>
+      <section className={S.container}>
         <Image
           className={S.infoBarImage}
           loader={TMDBPosterLoader}
@@ -70,67 +38,7 @@ export default async function Elenco({ params }: ElencoProps) {
         </div>
       </section>
 
-      <section className={[S.container, S.containerListNames].join(' ')}>
-        <button className={S.listNameTab}>
-          Elenco <span className={S.listQuant}>{`(${castList.length})`}</span>
-        </button>
-
-        <button className={S.listNameTab}>
-          Equipe Técnica <span className={S.listQuant}>{`(${crewList.length})`}</span>
-        </button>
-
-        <button className={S.listNameTab}>
-          Todos <span className={S.listQuant}>{`(${castList.length + crewList.length})`}</span>
-        </button>
-      </section>
-
-      <section className={[S.container, S.containerLists].join(' ')}>
-        <div className={S.listContent}>
-          <h2 className={S.listName}>Elenco</h2>
-
-          <ul className={S.listWrapper}>
-            {castList.map((item) => {
-              const id = item.id
-              const image = item.profile_path
-              const name = item.name
-              const subName = item.character
-
-              return <TeamCard key={`cast-${id}`} id={id} image={image} name={name} subName={subName} />
-            })}
-          </ul>
-        </div>
-
-        <hr className={S.division} />
-
-        <div className={S.listContent}>
-          <h2 className={S.listName}>Equipe Técnica</h2>
-
-          {crewListFormatted.map((depart) => {
-            const name = depart[0] as keyof typeof DICTIONARY_CREW_DEPARTMENT
-            const list = depart[1] as Crew[]
-
-            return (
-              <div key={`department-${name}`} className={S.listDepart}>
-                <h3 className={S.departName}>
-                  {DICTIONARY_CREW_DEPARTMENT[name] ? DICTIONARY_CREW_DEPARTMENT[name] : name}
-                  <span className={S.listDepartQuant}>{`(${list.length})`}</span>
-                </h3>
-
-                <ul className={S.listWrapper}>
-                  {list.map((item) => {
-                    const id = item.id
-                    const image = item.profile_path
-                    const name = item.name
-                    const subName = item.job
-
-                    return <TeamCard key={`crew-${id}`} id={id} image={image} name={name} subName={subName} />
-                  })}
-                </ul>
-              </div>
-            )
-          })}
-        </div>
-      </section>
+      <TeamTab credits={credits} />
     </main>
   )
 }
