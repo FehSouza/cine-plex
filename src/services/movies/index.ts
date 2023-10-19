a10dabd097128bde6e8b18b6ddf7f374abbf1418
FehSouza <engcivil.fernandasouza@gmail.com>
2023-10-19 13:49:15 -0300

    fix(person-page): new request url movie credits

--- src/services/movies/index.ts
+++ src/services/movies/index.ts
@@ -117,12 +117,9 @@ export async function getSocialMedia(id: number) {
 
 export async function getPersonCredits(id: string) {
   try {
-    const res = await fetch(`https://api.themoviedb.org/3/person/${Number(id)}/combined_credits?language=pt-BR`, options)
+    const res = await fetch(`https://api.themoviedb.org/3/person/${id}/movie_credits?language=pt-BR`, options)
     const response = (await res.json()) as PersonCredits
-    const filteredListCast = response.cast.filter((cast) => cast.media_type !== 'tv')
-    const filteredListCrew = response.crew.filter((crew) => crew.media_type !== 'tv')
-
-    return { id: response.id, cast: filteredListCast, crew: filteredListCrew }
+    return response
   } catch (error) {
     console.log('function getPersonCredits with Error: ', error)
     return { id: id, cast: [], crew: [] }
