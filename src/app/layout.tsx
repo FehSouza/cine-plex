52961d68189ecc3b9d76201fcb17f523df5c0676
FehSouza <engcivil.fernandasouza@gmail.com>
2023-11-18 16:37:18 -0300

    feat(search): without zoom input focus

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,10 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt-BR" id="content" suppressHydrationWarning>
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+      </head>
+
       <body className={inter.className}>
         <Header />
         <div className={S.container}>
