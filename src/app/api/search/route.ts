e84bb3489ed6eb725e24ab6b2178668e46c1e434
FehSouza <engcivil.fernandasouza@gmail.com>
2023-11-20 19:07:11 -0300

    fix(search): response next

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -1,10 +1,11 @@
 import { getSearch } from '@/services'
+import { NextResponse } from 'next/server'
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url)
   const query = searchParams.get('q')
-  if (!query) return Response.json({ page: 0, results: [], total_pages: 0, total_results: 0 })
+  if (!query) return NextResponse.json({ page: 0, results: [], total_pages: 0, total_results: 0 })
 
   const result = await getSearch({ query, page: '1' })
-  return Response.json(result)
+  return NextResponse.json(result)
 }
