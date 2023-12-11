5f317a75ceef13d4d42d166d0c36b9c4e869a8b8
FehSouza <engcivil.fernandasouza@gmail.com>
2023-12-11 19:06:06 -0300

    chose(head): meta tag viewport

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,9 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt-BR" id="content" suppressHydrationWarning>
+      <head>
+        <meta key="viewport-default" name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+      </head>
       <body className={inter.className}>
         <Header />
         <div className={S.container}>
