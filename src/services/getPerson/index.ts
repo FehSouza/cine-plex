181af7003c6c2ef1ab3d90b00d0d90c7d10e8070
FehSouza <fernanda.souza142010@gmail.com>
2024-05-15 20:51:09 -0300

    refactor: server getPerson

--- src/services/getPerson/index.ts
+++ src/services/getPerson/index.ts
@@ -4,7 +4,11 @@ import { HttpResponse, http } from 'msw'
 import { optionsOneDay } from '../configs'
 
 export async function getPerson(id: string) {
-  const response = await fetch(`https://api.themoviedb.org/3/person/${id}?language=pt-BR`, optionsOneDay)
+  const searchParams = new URLSearchParams({
+    language: 'pt-BR',
+  })
+
+  const response = await fetch(`https://api.themoviedb.org/3/person/${id}?${searchParams.toString()}`, optionsOneDay)
   const result = (await response.json()) as Person
   return result
 }
