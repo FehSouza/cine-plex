cebdce9695467f2a5c7d3f77abadb899e87e5955
FehSouza <fernanda.souza142010@gmail.com>
2024-05-21 12:05:43 -0300

    test: server getSearchResults

--- src/services/getSearchResults/index.ts
+++ src/services/getSearchResults/index.ts
@@ -1,5 +1,27 @@
-export const getSearchResults = async (query: string) => {
-  const response = await fetch(`/api/search?q=${query}`)
+import {
+  MOCK_GET_SEARCH_PERSON_QUERY_A,
+  MOCK_GET_SEARCH_PERSON_QUERY_ANY,
+  MOCK_GET_SEARCH_QUERY_A,
+  MOCK_GET_SEARCH_QUERY_ANY,
+} from '@/mocks'
+import { HttpResponse, http } from 'msw'
+
+const URL = '/api/search'
+
+export const getSearchResults = async (props: { query: string } = { query: '' }) => {
+  const searchParams = new URLSearchParams({
+    q: props.query,
+  })
+
+  const response = await fetch(`${URL}?${searchParams.toString()}`)
   const result = await response.json()
   return result
 }
+
+export const mockGetSearchResults = http.get(URL, ({ request }) => {
+  const searchParams = new URLSearchParams(request.url)
+  const query = searchParams.get(`${URL}?q`)
+  console.log({ query, searchParams })
+  if (query === 'a') return HttpResponse.json({ movies: MOCK_GET_SEARCH_QUERY_A, people: MOCK_GET_SEARCH_PERSON_QUERY_A })
+  return HttpResponse.json({ movies: MOCK_GET_SEARCH_QUERY_ANY, people: MOCK_GET_SEARCH_PERSON_QUERY_ANY })
+})
