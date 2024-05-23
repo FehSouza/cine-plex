ff233b4b5c7828aa10d1b33033fb08ef3c24e339
FehSouza <fernanda.souza142010@gmail.com>
2024-05-23 17:35:46 -0300

    merge

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig, defaultExclude, coverageConfigDefaults } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { coverageConfigDefaults, defaultExclude, defineConfig } from 'vitest/config'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,6 +8,7 @@ export default defineConfig({
   test: {
     environment: 'jsdom',
     setupFiles: ['./testConfig.ts', './jest-setup.ts'],
+    exclude: ['**/tests', ...defaultExclude],
     coverage: {
       exclude: [
         '**/src/components/index.ts**',
@@ -16,8 +17,8 @@ export default defineConfig({
         '**/src/mocks/index.ts**',
         '**/src/services/index.ts**',
         '**/src/utils/index.ts**',
+        '**/tests',
         ...coverageConfigDefaults.exclude,
-        '**/tests'
       ],
     },
   },
