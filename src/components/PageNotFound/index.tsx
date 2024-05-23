5abbaa3362ceca014e7e945a15bdcaf08930759c
FehSouza <fernanda.souza142010@gmail.com>
2024-05-23 19:19:49 -0300

    test: component PageNotFound

--- src/components/PageNotFound/index.tsx
+++ src/components/PageNotFound/index.tsx
@@ -3,13 +3,17 @@ import S from './styles.module.scss'
 
 export const PageNotFound = () => {
   return (
-    <main className={S.main}>
+    <main data-testid="page-not-found" className={S.main}>
       <div className={S.container}>
-        <h1 className={S.title}>Ops! Não encontramos a página que você procura</h1>
+        <h1 data-testid="page-not-found-title" className={S.title}>
+          Ops! Não encontramos a página que você procura
+        </h1>
 
-        <span className={S.text}>Infelizmente você tentou solicitar uma página que não existe em nosso catálogo...</span>
+        <span data-testid="page-not-found-text" className={S.text}>
+          Infelizmente você tentou solicitar uma página que não existe em nosso catálogo...
+        </span>
 
-        <Link className={S.link} href="/">
+        <Link data-testid="page-not-found-link" className={S.link} href="/">
           Voltar para a Home
         </Link>
       </div>
